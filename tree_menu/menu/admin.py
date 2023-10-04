db8bc8166bc5fd3fa036ca4bc423141c9f02668a
Вячеслав Менюхов <menyukhov@bk.ru>
2023-10-04 20:50:20 +0300

    Add menu models.

--- tree_menu/menu/admin.py
+++ tree_menu/menu/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Menu, MenuItem
 
-# Register your models here.
+
+class MenuItemInline(admin.TabularInline):
+    model = MenuItem
+    extra = 0
+
+
+@admin.register(Menu)
+class MenuAdmin(admin.ModelAdmin):
+    inlines = [MenuItemInline]
--- tree_menu/menu/models.py
+++ tree_menu/menu/models.py
@@ -1,1 +1,46 @@
 from django.db import models
+
+
+class Menu(models.Model):
+    name = models.CharField(
+        max_length=100,
+        verbose_name='name'
+    )
+
+    class Meta:
+        verbose_name = 'Menu'
+        verbose_name_plural = 'Menus'
+
+    def __str__(self):
+        return self.name
+
+
+class MenuItem(models.Model):
+    menu = models.ForeignKey(
+        Menu,
+        related_name='items',
+        verbose_name='menu',
+        on_delete=models.CASCADE
+    )
+    parent = models.ForeignKey(
+        'self',
+        verbose_name='parent',
+        related_name='child',
+        blank=True, null=True,
+        on_delete=models.PROTECT
+
+    )
+    name = models.CharField(
+        max_length=100,
+        verbose_name='name'
+    )
+    url = models.URLField(
+        verbose_name='url'
+    )
+
+    class Meta:
+        verbose_name = 'Menu item'
+        verbose_name_plural = 'Menu items'
+
+    def __str__(self):
+        return self.name
